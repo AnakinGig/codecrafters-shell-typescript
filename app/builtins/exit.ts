9cf1704e797504b07c88b09a001de37d1e6a54e4
AnakinGig <gigantanakin19@gmail.com>
2026-06-30 14:05:47 +0200

    codecrafters submit [skip ci]

--- app/builtins/exit.ts
+++ app/builtins/exit.ts
@@ -1,9 +1,15 @@
 import { type Builtin } from "../shell/types";
+import { writeHistoryToFile } from "../shell/history";
+import { rl } from "../index";
 
 export const exit: Builtin = {
   minArgs: 0,
   maxArgs: 0,
   execute: (args, redirects) => {
+    if (process.env.HISTFILE) {
+      writeHistoryToFile(process.env.HISTFILE);
+    }
+    rl.close();
     process.exit(0);
   }
 };
